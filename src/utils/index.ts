aee6eb2aa649d56c89d1b7eac25aebfd09c42ec4
Dương Đỗ <dustin.do95@gmail.com>
2019-03-14 15:55:35 +0700

    feat: auto create UPLOAD_DIR if `process.env.UPLOAD_DIR` set but the folder doesn't exist

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,22 @@ import { s3 } from 'functions/files'
 import logger from './logger'
 
 export const serverStartingHealthCheck = async () => {
+  if (!process.env.UPLOAD_DIR) {
+    logger.error(`process.env.UPLOAD_DIR is not set.`)
+    throw new Error(`process.env.UPLOAD_DIR is not set.`)
+  }
+
+  if (process.env.UPLOAD_DIR.length && !fs.existsSync(process.env.UPLOAD_DIR)) {
+    fs.mkdirSync(process.env.UPLOAD_DIR)
+    logger.info(
+      `Found process.env.UPLOAD_DIR=${process.env.UPLOAD_DIR} but ${
+        process.env.UPLOAD_DIR
+      } directory doesn't exist. We will auto-create ${
+        process.env.UPLOAD_DIR
+      } for you.`,
+    )
+  }
+
   if (!(process.env.UPLOAD_DIR && fs.existsSync(process.env.UPLOAD_DIR))) {
     logger.error('process.env.UPLOAD_DIR is not valid.')
     throw new Error('process.env.UPLOAD_DIR is not valid.')
